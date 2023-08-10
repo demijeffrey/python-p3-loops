72ce1e6381fb61ed8ac3ff7884a317bfba96365d
Demaris Ashmore <demiashmore@yahoo.com>
2023-08-10 11:26:38 -0600

    Completed lab"

--- lib/looping.py
+++ lib/looping.py
@@ -3,11 +3,40 @@
 def happy_new_year():
     # code goes here!
     pass
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
     pass
+    squared = [num * num for num in int_list]
+    return squared
 
 def fizzbuzz():
     # code goes here!
     pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(f"{i}")
+
+
+#### FIZZBUZZ SOLUTION ####
+# def fizzbuzz():
+#     for i in range(1, 101):
+#         if not i % 15:
+#             print("FizzBuzz")
+#         elif not i % 5:
+#             print("Buzz")
+#         elif not i % 3:
+#             print("Fizz")
+#         else:
+#             print(i)
